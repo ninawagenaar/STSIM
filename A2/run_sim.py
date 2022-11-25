98f8aeaa6acba80c38c661f6302f907fe9a338d8
Nina Wagenaar <nina_wagenaar@live.nl>
2022-11-25 14:40:13 +0100

    started setup code repository

---
+++ A2/run_sim.py
@@ -0,0 +1,3 @@
+import numpy as np
+import simpy as sp
+import matplotlib.pyplot as plt
---
+++ A2/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import simpy as sp
+import matplotlib.pyplot as plt
+
+
+# def M(rate):
+    
