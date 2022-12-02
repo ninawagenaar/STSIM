0a6a5c6b9e645f5d669ebe369acbe48d4ec58a57
karinleanne <karin.leanne@hotmail.com>
2022-12-02 11:17:56 +0100

    fixed bug

--- A2/m_m_n.py
+++ A2/m_m_n.py
@@ -5,11 +5,6 @@ import random
 import simpy
 import numpy as np
 
-
-RANDOM_SEED = 42
-SIM_TIME = 2000
-
-
 class Servers(object):
     """
     
@@ -19,12 +14,13 @@ class Servers(object):
         self.machine = simpy.Resource(env, num_servers)
         self.capacity = capacity
 
-    def serve(self):
+    def serve(self, name):
         """
         
         """
         servetime = random.expovariate(self.capacity)
         yield self.env.timeout(servetime)
+
 
 def customer(env, name, s, wait_times):
     """
@@ -32,14 +28,16 @@ def customer(env, name, s, wait_times):
     """
     with s.machine.request() as request:
         start_wait = env.now
+        #print('%s enters the at %.2f.' % (name, env.now))
         yield request
         end_wait = env.now
+        #print('%s served at %.2f.' % (name, env.now))
         wait_time = [end_wait - start_wait]
-
         # Record witing time
         wait_times.append(wait_time)
 
-        yield env.process(s.serve())
+        yield env.process(s.serve(name))
+        #print('%s leaves at %.2f.' % (name, env.now))
 
 
 
@@ -62,24 +60,29 @@ def setup(env, num_servers, num_customers, capacity, arrival_rate, wait_times):
         env.process(customer(env, 'Customer %d' % i, servers, wait_times))
 
 
-def run_simulation(num_servers, num_customers, capacity, arrival_rate):
-    avg_wait_times = np.empty(len(num_servers))
+def run_simulation(num_servers, num_simulations, num_customers, capacity, arrival_rate):
+    avg_wait_times = np.zeros((len(num_servers), num_simulations))
+
     for idx_n in range(len(num_servers)):
-        arrival_rate = arrival_rate * num_servers[idx_n]
-        wait_times = []
-        # Create an environment and start the setup process
-        env = simpy.Environment()
-        env.process(setup(env, num_servers[idx_n], num_customers, capacity, arrival_rate, wait_times))
-        # Execute!
-        env.run()
-        avg_wait_times[idx_n] = np.mean(wait_times) 
+        for sim in range(num_simulations):
+            wait_times = []
+            arrival_rate_n = arrival_rate * num_servers[idx_n]
+            # Create an environment and start the setup process
+            env = simpy.Environment()
+            env.process(setup(env, num_servers[idx_n], num_customers, capacity, arrival_rate_n, wait_times))
+            # Execute!
+            env.run()
+            avg_wait_times[idx_n][sim] = np.mean(wait_times)
 
-    # Print record
-    print(avg_wait_times)
+    return avg_wait_times
 
-random.seed(RANDOM_SEED)  # This helps reproducing the results 
-num_servers = [1, 2, 4]
-num_customers = 30000
+num_servers = [1,2,4]
+num_simulations = 100
+num_customers = 50
 capacity = 2
 arrival_rate = 1.9
-run_simulation(num_servers, num_customers, capacity, arrival_rate)
+avg_wait_times = run_simulation(num_servers, num_simulations, num_customers, capacity, arrival_rate)
+
+# Results
+mean_over_sim = [np.mean(result_n_servers) for result_n_servers in avg_wait_times]
+print(mean_over_sim)
