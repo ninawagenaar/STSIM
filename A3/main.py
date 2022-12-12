8c690c81fa915a1b002754e7bfcf5c877b984060
karinleanne <karin.leanne@hotmail.com>
2022-12-13 00:34:47 +0100

    worked on simulated annealing

--- A3/main.py
+++ A3/main.py
@@ -2,14 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import read_tsp as tsp
 import solution_search as sol
-            
+import plotting as plotting
+
+def run_simulations(tspProblem, simulations, max_iter, probability, temperature):
+    cost_over_iter = []
+    for simulation in range(simulations):
+
+        print("simulation:", simulation)
+
+        search_alg_sa = sol.search_alg("simulatedannealing", tspProblem.dimension, max_iter)
+        search_alg_sa.simulatedannealing(tspProblem, max_iter, probability, temperature)
+        cost_over_iter.append(search_alg_sa.history_cost)
+
+    cost_over_iter = np.asarray(cost_over_iter)
+    plotting.plot_cost_over_iter(cost_over_iter)
+    
 
 def main():
     filename = 'eil51.tsp.txt'
-    problem = tsp.read_problem_tsp(filename) 
+    tspProblem = tsp.read_problem_tsp(filename) 
     #problem.plot_problem()
-    solution = sol.solution("hillclimbing", len(problem.node_coord_section[:,0]))
-    solution.hillclimbing(problem)
+
+    max_iter = 10000
+    simulations = 20
+    temperature = 0.9
+    probability = 0.3
+    run_simulations(tspProblem, simulations, max_iter, probability, temperature)
+
 
 if __name__ == "__main__":
     main()
--- A3/read_tsp.py
+++ A3/read_tsp.py
@@ -1,11 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 class tspProblem:
 
     def __init__(self, variables, node_coord_section):
-        self.variables = variables
+        self.name = variables['NAME']
+        self.dimension = int(variables['DIMENSION'])
         self.node_coord_section = node_coord_section
+        self.distances = self.get_distance_matrix()
+
+    def get_distance_matrix(self):
+        distances = np.empty((self.dimension, self.dimension))
+        for i in self.node_coord_section[:,0]:
+            for j in self.node_coord_section[:,0]:
+                if i == j:
+                    distances[i-1][j-1] = 0
+                else:
+                    x1 = self.node_coord_section[i-1,1]
+                    y1 = self.node_coord_section[i-1,2]
+                    coor_node1 = (x1, y1)
+
+                    x2 = self.node_coord_section[j-1,1]
+                    y2 = self.node_coord_section[j-1,2] 
+                    coor_node2 = (x2, y2)
+
+                    distances[i-1][j-1] = math.dist(coor_node1, coor_node2)
+        return distances
+                
 
     def plot_problem(self):
         plt.scatter(self.node_coord_section[:,1], self.node_coord_section[:,2])
@@ -14,6 +36,7 @@ class tspProblem:
         plt.title("Vizualization of nodes")
         plt.grid()
         plt.show()
+
 
 def read_problem_tsp(filename):
     variables = {}
